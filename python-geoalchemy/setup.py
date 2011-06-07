0546075e430d3d6daa31b9331de266d37a19bba8
Muharem Hrnjadovic <mh@foldr3.com>
2011-06-07 08:18:10 +0200

    started packaging python-geoalchemy

---
+++ python-geoalchemy/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.5'
+
+setup(name='python-geoalchemy',
+      version=version,
+      description="Using SQLAlchemy with Spatial Databases",
+      long_description=open('README.txt').read(),
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Environment :: Plugins",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Intended Audience :: Information Technology",
+          "License :: OSI Approved :: MIT License",
+          "Topic :: Scientific/Engineering :: GIS"
+      ],
+      keywords='geo gis sqlalchemy orm',
+      author='Sanjiv Singh',
+      author_email='singhsanjivk@gmail.com',
+      maintainer='Eric Lemoine',
+      maintainer_email='eric.lemoine@camptocamp.com',
+      url='http://geoalchemy.org/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests', "doc"]),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'SQLAlchemy>=0.6.1',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
