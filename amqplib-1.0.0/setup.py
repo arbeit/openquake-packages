a1cd86feeb244f92382ba1d3fae402d2f6765bc9
Muharem Hrnjadovic <mh@foldr3.com>
2011-08-26 06:10:41 +0200

    added the python-amqplib package

---
+++ amqplib-1.0.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+Python Distutils setup for for amqp.  Build and install with
+
+    python setup.py install
+
+2007-11-10 Barry Pederson <bp@barryp.org>
+
+"""
+
+import sys
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    # 2.x
+    from distutils.command.build_py import build_py
+
+setup(name = "amqplib",
+      description = "AMQP Client Library",
+      version = "1.0.0",
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          ],
+      license = "LGPL",
+      author = "Barry Pederson",
+      author_email = "bp@barryp.org",
+      url = "http://code.google.com/p/py-amqplib/",
+      packages = ['amqplib', 'amqplib.client_0_8'],
+      cmdclass = {'build_py':build_py},
+     )
