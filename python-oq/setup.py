7643cd978cbacb8670e9735498bce1898d607928
Muharem Hrnjadovic <mh@foldr3.com>
2012-04-02 15:41:20 +0200

    refreshed setup.py

--- python-oq/setup.py
+++ python-oq/setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name = "openquake",
-    version = "0.6.0",
+    version = "0.6.1",
     author = "The OpenQuake team",
     author_email = "info@openquake.org",
     description = ("Computes hazard, risk and socio-economic impact of "
